e3baf4f789fb4c1a8bd44d0e7054638c35f045cf
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-01-27 16:52:10 -0800

    Example of using request in a promise friendly way

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import * as cheerio from 'cheerio'
 import * as csv from 'csv'
 import {Promise} from 'bluebird'
 
-Promise.promisifyAll(request)
 Promise.promisifyAll(cheerio)
 Promise.promisifyAll(csv)
+Promise.promisifyAll(request, {multiArgs: true})
 
 console.log('Hello world')
+
+request.getAsync('https://www.google.com/')
+  .then(([code, body]) => {
+    console.log(`Code: ${code.statusCode}`)
+    console.log(`Body: ${body}`)
+  })
