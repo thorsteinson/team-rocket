28fefdcb2cb4e3bb6c2557f94098519435ab29d5
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-01-28 18:49:12 -0800

    Delayed serialy requests working with bluebird
    Had to figure out how to use mapSeries together with Promise.delay in
    order to prevent promises from executing asynchronously in parallel.
    
    This is now capable of fetching all pokemon.
    
    Last step is getting CSV parsing to work.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import * as request from 'request'
 import {load} from 'cheerio'
 import * as csv from 'csv'
 import {Promise} from 'bluebird'
+import {default as _} from 'lodash'
 
 import {parsePokemon} from './pokemon'
 
@@ -9,69 +10,127 @@ import {parsePokemon} from './pokemon'
 Promise.promisifyAll(csv)
 Promise.promisifyAll(request, {multiArgs: true})
 
-// const BASE_SCRAPE_URL = 'http://pokemondb.net/pokedex/national'
+const BASE_SCRAPE_URL = 'http://pokemondb.net/pokedex/national'
+const BASE_URI = 'http://pokemondb.net'
 
-// Idea: Create an array of promises that are serially executed with delays between each
-// Each request gets parsed into data
+// SERIALLY fetches each pokemon, with a 500ms delay inbetween each request
+Promise.mapSeries(requests, promise=> {
+  const req = promise()
+    .then(p => {
+      // Logs out pokemon
+      console.log(p)
+    })
+  return Promise.delay(500).then(req)
+})
 
-// Real example of a page, just for testing purposes
-const BULBASAUR = 'http://pokemondb.net/pokedex/bulbasaur'
-
-request.getAsync(BULBASAUR)
+// Get all the links on the root page
+const requests = request.getAsync(BASE_SCRAPE_URL)
   .then(([code, body]) => {
-    // Use cheerio to parse the web page
     const $ = load(body)
 
-    const pokedex = $("h2:contains('data')").next().find('td')
-    const idNum = $(pokedex.get(0)).text()
-    const type = $(pokedex.get(1)).text()
-    const species = $(pokedex.get(2)).text()
-    const height = $(pokedex.get(3)).text()
-    const weight = $(pokedex.get(4)).text()
-
-    const training = $("h2:contains('Training')").next().find('td')
-    const ev = $(training.get(0)).text()
-    const catchRate = $(training.get(1)).text()
-    const baseHappiness = $(training.get(2)).text()
-    const baseExp = $(training.get(3)).text()
-    const growthRate = $(training.get(4)).text()
-
-    const breeding = $("h2:contains('Breeding')").next().find('td')
-    const gender = $(breeding.get(1)).text()
-    const eggCycles = $(breeding.get(2)).text()
-
-    const stats = $("h2:contains('stats')").next()
-    const hp = stats.find("th:contains('HP')").next().text()
-    const attack = stats.find("th:contains('Attack')").next().text()
-    const defense = stats.find("th:contains('Defense')").next().text()
-    const speed = stats.find("th:contains('Speed')").next().text()
-    const specialAttack = stats.find("th:contains('Sp. Atk')").next().text()
-    const specialDefense = stats.find("th:contains('Sp. Def')").next().text()
-    const total = stats.find("th:contains('Total')").next().text()
-
-    const pokemon = parsePokemon({
-      id: idNum,
-      species,
-      type,
-      height,
-      weight,
-      ev,
-      catchRate,
-      baseExp,
-      growthRate,
-      eggCycles,
-      baseHappiness,
-      gender,
-      hp,
-      attack,
-      specialAttack,
-      defense,
-      specialDefense,
-      speed,
-      total,
-      name: 'bulbasaur',
-      generation: '1'
-    })
-
-    console.log(pokemon)
+    // List of all the pokemons is contained in this div
+    const pokeContainer = $('.infocard-tall-list')
+    // Gets links to each pokemon page, and the generation / name
+    return _
+      .chain(pokeContainer.find('.infocard-tall').toArray())
+      .take(2)
+      .map(elem => {
+        const tmp = $(elem)
+        const pokemon = tmp.find('a.ent-name')
+        const obj = {
+          name: pokemon.text(),
+          href: BASE_URI + pokemon.attr('href'),
+          generation: getGeneration(pokemon.find('small').first().text().substring(1))
+        }
+        return obj
+      })
+      // Only go up to gen 5, since data is incomplete for 6 at the moment
+      .filter(pokemon => pokemon.generation <= 5)
+      .map(pokemon => {
+        return function () {
+          return fetchPokemon(pokemon)
+        }
+      })
+      .value()
   })
+
+// Maps over a collection with a delay
+function throttledMap (array, f, delay) {
+  const results = []
+  const idx = 0
+
+  function helper(value, array, f) {
+
+  }
+}
+
+
+// Given an ID, give back the generation
+function getGeneration (id) {
+  if (id <= 151) return 1
+  else if (id <= 251) return 2
+  else if (id <= 386) return 3
+  else if (id <= 493) return 4
+  else if (id <= 649) return 5
+  else return 6
+}
+
+// Returns a promise that resolves a pokemon object
+function fetchPokemon ({name, href, generation}) {
+  return request.getAsync(href)
+    .then(([code, body]) => {
+      // Use cheerio to parse the web page
+      const $ = load(body)
+
+      const pokedex = $("h2:contains('data')").next().find('td')
+      const idNum = $(pokedex.get(0)).text()
+      const type = $(pokedex.get(1)).text()
+      const species = $(pokedex.get(2)).text()
+      const height = $(pokedex.get(3)).text()
+      const weight = $(pokedex.get(4)).text()
+
+      const training = $("h2:contains('Training')").next().find('td')
+      const ev = $(training.get(0)).text()
+      const catchRate = $(training.get(1)).text()
+      const baseHappiness = $(training.get(2)).text()
+      const baseExp = $(training.get(3)).text()
+      const growthRate = $(training.get(4)).text()
+
+      const breeding = $("h2:contains('Breeding')").next().find('td')
+      const gender = $(breeding.get(1)).text()
+      const eggCycles = $(breeding.get(2)).text()
+
+      const stats = $("h2:contains('stats')").next()
+      const hp = stats.find("th:contains('HP')").next().text()
+      const attack = stats.find("th:contains('Attack')").next().text()
+      const defense = stats.find("th:contains('Defense')").next().text()
+      const speed = stats.find("th:contains('Speed')").next().text()
+      const specialAttack = stats.find("th:contains('Sp. Atk')").next().text()
+      const specialDefense = stats.find("th:contains('Sp. Def')").next().text()
+      const total = stats.find("th:contains('Total')").next().text()
+
+      return parsePokemon({
+        id: idNum,
+        species,
+        type,
+        height,
+        weight,
+        ev,
+        catchRate,
+        baseExp,
+        growthRate,
+        eggCycles,
+        baseHappiness,
+        gender,
+        hp,
+        attack,
+        specialAttack,
+        defense,
+        specialDefense,
+        speed,
+        total,
+        name,
+        generation
+      })
+    })
+}
